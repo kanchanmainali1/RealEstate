552ab807bf665af8b5268e4cd8969140e4d6b3aa
kanchanmainali1 <kanchanmainali1@gmail.com>
2024-11-23 22:45:54 +0545

    ContinueMapContainer

--- src/routes/layout/layout.jsx
+++ src/routes/layout/layout.jsx
@@ -1,6 +1,6 @@
 
 import "./layout.scss";
-import Navbar from "../../components/navbar/navbar"
+import Navbar from "../../components/navbar/Navbar"
 import { Outlet } from "react-router-dom";
 
 function Layout() {
